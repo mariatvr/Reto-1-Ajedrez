719fde18a4901a367a81fd11c47453c01fa0ab16
miken <mikenorts@gmail.com>
2025-12-16 11:31:01 +0100

    Actualización Concepto del Proyecto

--- src/Tablero.java
+++ src/Tablero.java
@@ -6,15 +6,24 @@ public class Tablero {
         this.tablero= new Pieza[8][8];
     }
 
-    public void ColocarPiezas (String posicion){
+    public void colocarPiezas (String posicion, boolean blancas){
 
     }
 
-    public Pieza getTablero(Posicion p) {
+    /**
+    Esta función determina si el camino está despejado desde una posición inicial p1 hasta
+    **/
+    public boolean caminoLibre (Posicion p1, Posicion p2, int dir){
+        boolean libre = true;
+
+        return libre;
+    }
+
+    public Pieza getPosicion(Posicion p) {
         return tablero[p.getFila()][p.getColumna()];
     }
 
-    public void setTablero(Pieza pieza, Posicion p) {
+    public void setPieza(Pieza pieza, Posicion p) {
         this.tablero[p.getFila()][p.getColumna()] = pieza;
     }
 
