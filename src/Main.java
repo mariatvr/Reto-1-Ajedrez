ef6ed895983cc6f51f109b2018dcc0a095afbb54
Roque Mas <roqmascer@alu.edu.gva.es>
2025-12-18 10:15:45 +0100

    Añadiendo la función de comprobar el formato de la posición inicial

--- src/Main.java
+++ src/Main.java
@@ -58,4 +58,114 @@ public class Main {
             System.out.println("Color introducido incorrecto.");
         }
     }
+
+    public static boolean posicionIni(String piezas) {
+        /*
+        Primero de todo, valida que no contiene símbolos antes de comprobar el String por trozos.
+         */
+        boolean valido = false;
+        for (int i = 0; i < piezas.length(); i++) {
+            char c = piezas.charAt(i);
+            /*
+            Si el caracter actual que recorre el string es una letra o un número es válido.
+            Lo mismo pasa si contiene un espacio, que sería el separador de cada movimiento.
+             */
+            if (Character.isLetterOrDigit(c) || c == ' ') {
+                valido = true;
+            } else {
+                valido = false;
+            }
+        }
+        String trozo = "";
+        int ultChar = 0;
+        for (int j = 0; j < piezas.length(); j++) {
+
+            /*
+            Se "trocea" el string para validar cada uno de los trozos.
+             */
+            for (;ultChar < piezas.length(); ultChar++) {
+                char c = piezas.charAt(ultChar);
+                /*
+                Si contiene un espacio el caracter actual, se almacena el trozo de una pieza y del movimiento.
+                 */
+                if (c == ' ') {
+                    break;
+                }
+                trozo = trozo + c;
+            }
+
+                /*
+                Si el trozo es de 3 de longitud, es decir, que tiene una pieza y un movimiento, se comprueba que es correcto.
+                 */
+            if (trozo.length() == 3) {
+                /*
+                Se comprueba que la primera sea mayúscula (de acuerdo a la notación del ajedrez).
+                 */
+                if (trozo.charAt(0) == trozo.toUpperCase().charAt(0)) {
+                    /*
+                    Ahora se comprueba que el siguiente caracter sea una letra.
+                     */
+                    if (Character.isLetter(trozo.charAt(1))) {
+                        /*
+                        Por último, se comprueba que el siguiente caracter sea un número.
+                         */
+                        if (Character.isDigit(trozo.charAt(2))) {
+                            /*
+                            Si todo lo anterior se cumple en ese trozo en concreto, es válido (true).
+                             */
+                            valido = true;
+                            /*
+                            Pasa al siguiente trozo.
+                             */
+                            if (ultChar == piezas.length()) {
+                                break;
+                            } else {
+                                ultChar = ultChar + 1;
+                            }
+                        }
+                    }
+                }
+                /*
+                Si no, es que es un peón (solo 2 caracteres para el movimiento) y se valida.
+                 */
+            } else if (trozo.length() == 2) {
+                /*
+                Comprobamos que el primer caracter sea una letra.
+                 */
+                if (Character.isLetter(trozo.charAt(0))) {
+                    /*
+                    Aquí lo mismo pero comprobamos el número.
+                     */
+                    if (Character.isDigit(trozo.charAt(1))) {
+                        /*
+                        Si es válido, se le asigna true al boolean valido.
+                         */
+                        valido = true;
+                        /*
+                        Aquí lo mismo que antes, pasa al siguiente trozo disponible.
+                         */
+                        if (ultChar == piezas.length()) {
+                            break;
+                        } else {
+                            ultChar = ultChar + 1;
+                        }
+                    }
+                }
+
+                /*
+                Si no, el formato introducido es inválido, por lo tanto el boolean valido es false.
+                 */
+            } else {
+                valido = false;
+                /*
+                Una vez que sea inválido, se acaba la comprobación y finaliza la función.
+                 */
+                if (valido == false) {
+                    break;
+                }
+            }
+            trozo = "";
+        }
+        return valido;
+    }
 }
